85d157ea182b92a593dc7eabb0cbe03e365db0ab
刘光正 <rrefe4@163.com>
2020-09-25 11:23:36 +0800

    redis

--- oauth-server/src/main/java/com/krupp/cloud/oauth/ResourceServerConfigurer.java
+++ oauth-server/src/main/java/com/krupp/cloud/oauth/ResourceServerConfigurer.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -24,17 +26,20 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 @Configuration
 public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
 
+   @Autowired
+   private RedisConnectionFactory redisConnectionFactory;
+
     @Bean
-    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
-        return new JwtTokenStore(jwtAccessTokenConverter);
+    public TokenStore tokenStore() {
+        return new RedisTokenStore(redisConnectionFactory);
     }
 
     @Bean
-   public AccessTokenConverter accessTokenConverter() {
-       JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-       //jwtAccessTokenConverter.setKeyPair();
-       jwtAccessTokenConverter.setSigningKey("lgz");
-       return jwtAccessTokenConverter;
-   }
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        //对称加密方式 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
+        jwtAccessTokenConverter.setSigningKey("lgz");
+        return jwtAccessTokenConverter;
+    }
 
 }
